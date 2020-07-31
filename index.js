27f29a0777c7d93792fa8f4ad37c24315f9335c3
twojay1972 <68985365+twojay1972@users.noreply.github.com>
2020-07-31 14:45:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like twojay! hi2');
+    res.send('I like twojay! hi node test');
 });
 
 app.get('/start', function(req, res){
